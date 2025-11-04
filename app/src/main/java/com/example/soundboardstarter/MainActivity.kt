31302e39ee22a223a4d251920fd27f53fb90e1d9
Daxton Brown <nugnuts.the.2nd@gmail.com>
2025-11-04 09:45:27 -0800

    Add new buttons code

--- app/src/main/java/com/example/soundboardstarter/MainActivity.kt
+++ app/src/main/java/com/example/soundboardstarter/MainActivity.kt
@@ -14,11 +14,27 @@ class MainActivity : AppCompatActivity() {
     lateinit var buttonBb : Button
     lateinit var buttonB : Button
     lateinit var buttonC : Button
+    lateinit var buttonCs : Button
+    lateinit var buttonD : Button
+    lateinit var buttonDs : Button
+    lateinit var buttonE : Button
+    lateinit var buttonF : Button
+    lateinit var buttonFs : Button
+    lateinit var buttonG : Button
+    lateinit var buttonGs : Button
     lateinit var soundPool : SoundPool
     var aNote = 0
     var bbNote = 0
     var bNote = 0
     var cNote = 0
+    var csNote = 0
+    var dNote = 0
+    var dsNote = 0
+    var eNote = 0
+    var fNote = 0
+    var fsNote = 0
+    var gNote = 0
+    var gsNote = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,6 +57,14 @@ class MainActivity : AppCompatActivity() {
         bbNote = soundPool.load(this, R.raw.scalebb, 1)
         bNote = soundPool.load(this, R.raw.scaleb, 1)
         cNote =  soundPool.load(this, R.raw.scalec, 1)
+        csNote = soundPool.load(this, R.raw.scalecs, 1)
+        dNote = soundPool.load(this, R.raw.scaled, 1)
+        dsNote = soundPool.load(this, R.raw.scaleds, 1)
+        eNote =  soundPool.load(this, R.raw.scalee, 1)
+        fNote = soundPool.load(this, R.raw.scalef, 1)
+        fsNote = soundPool.load(this, R.raw.scalefs, 1)
+        gNote = soundPool.load(this, R.raw.scaleg, 1)
+        gsNote =  soundPool.load(this, R.raw.scalegs, 1)
     }
 
     private fun wireWidgets() {
@@ -48,6 +72,14 @@ class MainActivity : AppCompatActivity() {
         buttonBb = findViewById(R.id.button_main_bb)
         buttonB = findViewById(R.id.button_main_b)
         buttonC = findViewById(R.id.button_main_c)
+        buttonCs = findViewById(R.id.button_main_cs)
+        buttonD = findViewById(R.id.button_main_d)
+        buttonDs = findViewById(R.id.button_main_ds)
+        buttonE = findViewById(R.id.button_main_e)
+        buttonF = findViewById(R.id.button_main_f)
+        buttonFs = findViewById(R.id.button_main_fs)
+        buttonG = findViewById(R.id.button_main_g)
+        buttonGs = findViewById(R.id.button_main_gs)
     }
 
     private fun setListeners() {
@@ -56,6 +88,14 @@ class MainActivity : AppCompatActivity() {
         buttonBb.setOnClickListener(soundBoardListener)
         buttonB.setOnClickListener(soundBoardListener)
         buttonC.setOnClickListener(soundBoardListener)
+        buttonCs.setOnClickListener(soundBoardListener)
+        buttonD.setOnClickListener(soundBoardListener)
+        buttonDs.setOnClickListener(soundBoardListener)
+        buttonE.setOnClickListener(soundBoardListener)
+        buttonF.setOnClickListener(soundBoardListener)
+        buttonFs.setOnClickListener(soundBoardListener)
+        buttonG.setOnClickListener(soundBoardListener)
+        buttonGs.setOnClickListener(soundBoardListener)
     }
 
 
@@ -70,6 +110,14 @@ class MainActivity : AppCompatActivity() {
                 R.id.button_main_bb -> playNote(bbNote)
                 R.id.button_main_b -> playNote(bNote)
                 R.id.button_main_c -> playNote(cNote)
+                R.id.button_main_cs -> playNote(csNote)
+                R.id.button_main_d -> playNote(dNote)
+                R.id.button_main_ds -> playNote(dsNote)
+                R.id.button_main_e -> playNote(eNote)
+                R.id.button_main_f -> playNote(fNote)
+                R.id.button_main_fs -> playNote(fsNote)
+                R.id.button_main_g -> playNote(gNote)
+                R.id.button_main_gs -> playNote(gsNote)
             }
         }
     }
